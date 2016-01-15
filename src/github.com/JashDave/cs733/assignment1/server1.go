968349c5eca033541b3c89f3ced126eb07db7c8c
JashDave <jashdave23@gmail.com>
2016-01-15 08:08:07 +0530

    write in progress

--- src/github.com/JashDave/cs733/assignment1/server1.go
+++ src/github.com/JashDave/cs733/assignment1/server1.go
@@ -7,23 +7,99 @@ package main
 
 import "net"
 import "fmt"
+import "time"
 import "bufio"
-import "strings" 
+import "strings"
+import "strconv"
+import "io/ioutil"
 
 func doWrite(params []string, conn net.Conn) {
+	l := len(params)
+
+	if (l>4 || l<3){
+		conn.Write([]byte("ERR_CMD_ERR1\r\n"))
+		return
+	}
+
+	exptime := 0
+	filename := strings.TrimSuffix(params[1]," ")
+	numbytes,err := strconv.Atoi(strings.Trim(params[2]," \r\n"))
+
+	if err != nil {
+		conn.Write([]byte("ERR_CMD_ERR2 "+params[2]+" :end \r\n"))
+		return
+	}
+
+	if l == 4 {
+		exptime, err = strconv.Atoi(strings.Trim(params[3]," \r\n"))
+		if err != nil {
+			conn.Write([]byte("ERR_CMD_ERR3"+params[3]))
+			fmt.Println(err)
+			return
+		}
+	}
+
+	contents, err := bufio.NewReader(conn).ReadString('\n')	
+	if(err != nil) {
+		fmt.Printf("%v",err)
+		conn.Write([]byte("ERR_CMD_ERR4\r\n"))	  
+		return
+	}
+	contents = strings.TrimSuffix(strings.TrimSuffix(contents,"\n"),"\r")
+fmt.Println(contents)
+	current_time := time.Now()
+	
+	version := uint64(1)
+	data := fmt.Sprintf("%d\n%d\n%v\n%d\n%v",version,numbytes,current_time,exptime,contents)
+	err = ioutil.WriteFile("/cloud/assign1/"+filename,[]byte(data), 0644)
+	
+	conn.Write([]byte("OK "+string(version)+"\r\n"))
+/*
+	ioutil.WriteFile(file, data []byte, perm os.FileMode)
 	fmt.Println("Do write")
+*/
 }
 
 func doRead(params []string, conn net.Conn) {
+	conn.Write([]byte("Do Read\r\n"))
 	fmt.Println("Do Read")
 }
 
 func doCAS(params []string, conn net.Conn) {
+	conn.Write([]byte("Do CAS\r\n"))
 	fmt.Println("Do CAS")
 }
 
 func doDelete(params []string, conn net.Conn) {
+	conn.Write([]byte("Do Delete\r\n"))
 	fmt.Println("Do Delete")
+}
+
+func handleClient(conn net.Conn) {
+
+  for {
+    message, err := bufio.NewReader(conn).ReadString('\n')	
+	if(err != nil) {
+	  fmt.Printf("%v",err)
+	  break
+	}
+    fmt.Print("Message Received:", string(message))
+    var commandline []string = strings.SplitAfter(message, " ")
+    fmt.Printf("%q  \n",commandline)
+    switch strings.TrimSuffix(commandline[0]," ") {
+	case "write":
+		doWrite(commandline, conn)
+	case "read":
+		doRead(commandline, conn)
+	case "cas":
+		doCAS(commandline, conn)
+	case "delete":
+		doDelete(commandline, conn)
+	default:
+		conn.Write([]byte("ERR_INTERNAL\r\n"))
+	}
+  }
+
 }
 
 
@@ -31,27 +107,12 @@ func serverMain() {
 
   fmt.Println("Launching server...")
   ln, _ := net.Listen("tcp", ":8080")
-  conn, _ := ln.Accept()
-
   for {
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Message Received:", string(message))
-    var commandline []string = strings.SplitAfter(message, " ")
-    fmt.Printf("%q  \n",commandline)
-    switch commandline[0] {
-	case "write ":
-		doWrite(commandline, conn)
-	case "read ":
-		doRead(commandline, conn)
-	case "cas ":
-		doCAS(commandline, conn)
-	case "delete ":
-		doDelete(commandline, conn)
-	default:
-		fmt.Printf("ERR_INTERNAL\r\n")
+	conn, err := ln.Accept()
+	if(err != nil) {
+	  fmt.Printf("%v",err)
 	}
-    newmessage := strings.ToUpper(message)
-    conn.Write([]byte(newmessage + "\n"))
+	go handleClient(conn)
   }
 }
 
