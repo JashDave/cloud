4edf389e5b7970db004ba45c4d76c05d0b11fd7e
JashDave <jashdave23@gmail.com>
2016-03-10 00:00:32 +0530

    Initial Original

--- src/github.com/JashDave/cs733/assignment3/raft_node.go
+++ src/github.com/JashDave/cs733/assignment3/raft_node.go
@@ -1,113 +1,120 @@
 package raftnode
 
 import (
-//	"fmt"
-	"time"
+	//	"fmt"
 	"math"
-//	"errors"
-	"strconv"
-	"io/ioutil"
+	"time"
+	//	"errors"
 	"encoding/json"
-	"github.com/cs733-iitb/log"	
-	"github.com/cs733-iitb/cluster"
 	raftsm "github.com/JashDave/cs733/assignment3/assignment2"
+	"github.com/cs733-iitb/cluster"
+	"github.com/cs733-iitb/log"
+	"io/ioutil"
+	"strconv"
 )
 
-type Config struct{
-	Cluster []NetConfig
-	Id uint64 
-	LogDir string
-	ElectionTimeout uint64
+type Config struct {
+	Cluster          []NetConfig
+	Id               uint64
+	LogDir           string
+	ElectionTimeout  uint64
 	HeartbeatTimeout uint64
 }
 
-type NetConfig struct{
-	Id uint64
+type NetConfig struct {
+	Id   uint64
 	Host string
 	Port uint16
 }
 
-
-type TermVotedFor struct{
+type TermVotedFor struct {
 	CurrentTerm uint64
-	VotedFor uint64
+	VotedFor    uint64
 }
 
 type CommitInfo struct {
-	Data []byte
+	Data  []byte
 	Index uint64
-	Err error
+	Err   error
 }
 
 type RaftNode struct {
-	id uint64
-	majority uint64
+	id          uint64
+	majority    uint64
 	currentTerm uint64
-	votedFor uint64
+	votedFor    uint64
+	commitIndex uint64
+	leaderId    uint64
 
-	rnlog	*log.Log
-	server	cluster.Server
-	commitChan	chan CommitInfo
+	rnlog      *log.Log
+	server     cluster.Server
+	commitChan chan CommitInfo
 
-	sm	raftsm.StateMachine
-	smActionChan   chan raftsm.Action
-	smEventChan   chan raftsm.Event
-	
+	sm           *raftsm.StateMachine
+	smActionChan chan raftsm.Action
+	smEventChan  chan raftsm.Event
 }
 
+func GetServer(id int, Cluster []NetConfig) (cluster.Server, error) {
+	peers := make([]cluster.PeerConfig, len(Cluster))
+	for i, e := range Cluster {
+		peers[i] = cluster.PeerConfig{Id: int(e.Id), Address: e.Host + ":" + strconv.Itoa(int(e.Port))}
 
-func GetServer(id int,Cluster []NetConfig) (cluster.Server,error) {
-	peers := make([]cluster.PeerConfig,len(Cluster))
-	for i,e := range Cluster {
-		peers[i] = cluster.PeerConfig{Id: int(e.Id), Address: e.Host+":"+strconv.Itoa(int(e.Port))}
-		
 	}
-	cnf := cluster.Config{ Peers: peers }
-	return cluster.New(id,cnf)
+	cnf := cluster.Config{Peers: peers}
+	return cluster.New(id, cnf)
 }
 
-func CreateRaftNode(conf Config) (*RaftNode) {
+func CreateRaftNode(conf Config) *RaftNode {
 	rn := new(RaftNode)
-	rn.rnlog = log.Open(conf.LogDir)
-	rn.server = GetServer(int(conf.Id),conf.Cluster)
 
+	rn.leaderId = 0    //? 0 is invalid
+	rn.commitIndex = 0 //?verify
+	rnlog, err := log.Open(conf.LogDir)
+	if err != nil {
+		return nil
+	}
+	rn.rnlog = rnlog
 
-//--Init State Machine ---
-	peerIds := make([]uint64,len(conf.Cluster))
-	for i,e := range Cluster {
+	rn.server, err = GetServer(int(conf.Id), conf.Cluster)
+	if err != nil {
+		return nil
+	}
+
+	//--Init State Machine ---
+	peerIds := make([]uint64, len(conf.Cluster))
+	for i, e := range conf.Cluster {
 		if e.Id != conf.Id {
 			peerIds[i] = e.Id
 		}
 	}
-	rn.majority = math.Ceil(len(conf.Cluster)/2)	//? Assumes len is always odd
-	filename := "TermVotedFor" //? change required
+	rn.majority = uint64(math.Ceil(float64(len(conf.Cluster)) / 2.0)) //? Assumes len is always odd
+	filename := "TermVotedFor"                                        //? change required
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return nil	//error
+		return nil //error
 	}
 	var tvf TermVotedFor
 	err = json.Unmarshal(data, tvf)
 	if err != nil {
-		return nil	//error
+		return nil //error
 	}
 	rn.currentTerm = tvf.CurrentTerm
 	rn.votedFor = tvf.VotedFor
 	//le := make([]raftsm.LogEntry{},1) //? initilize from Log
-	rn.sm = InitStateMachine(conf.Id, peerIds, rn.majority, time.Duration(conf.ElectionTimeout)*time.Millisecond, time.Duration(conf.HeartbeatTimeout)*time.Millisecond, rn.currentTerm, rn.votedFor, /*//?*/[]raftsm.LogEntry{raftsm.LogEntry{0,nil,false}})
+	rn.sm = raftsm.InitStateMachine(conf.Id, peerIds, rn.majority, time.Duration(conf.ElectionTimeout)*time.Millisecond, time.Duration(conf.HeartbeatTimeout)*time.Millisecond, rn.currentTerm, rn.votedFor /*//?*/, []raftsm.LogEntry{raftsm.LogEntry{0, nil, false}})
 
-	
-	rn.smActionChan = sm.GetActionChan()
-	rn.smEventChan = sm.GetEventChan()
-	rn.commitChan = make(chan CommitInfo,100)
+	rn.smActionChan = *rn.sm.GetActionChannel()
+	rn.smEventChan = *rn.sm.GetEventChannel()
+	rn.commitChan = make(chan CommitInfo, 100)
+	return rn
 }
 
-
-
-func (rn *RaftNode) Append(data []byte ) {
-	rn.smEventChan <- raftsm.CreateEvent("Append","data",data)
+func (rn *RaftNode) Append(data []byte) {
+	rn.smEventChan <- raftsm.CreateEvent("Append", "data", data)
 }
 
-func (rn *RaftNode) GetCommitChannel() (* chan CommitInfo) {
+func (rn *RaftNode) GetCommitChannel() *chan CommitInfo {
 	return &rn.commitChan
 }
 
@@ -116,16 +123,16 @@ func (rn *RaftNode) CommittedIndex() uint64 {
 }
 
 func (rn *RaftNode) Get(index uint64) (error, []byte) {
-	data,err := rn.rnlog.Get(index)
+	data, err := rn.rnlog.Get(int64(index))
 	if err != nil {
-		return err
+		return err, nil
 	}
 	var le raftsm.LogEntry
 	err = json.Unmarshal(data, le)
 	if err != nil {
-		return err
+		return err, nil
 	}
-	return le.Data
+	return nil, le.Data
 }
 
 func (rn *RaftNode) Id() uint64 {
@@ -133,7 +140,7 @@ func (rn *RaftNode) Id() uint64 {
 }
 
 func (rn *RaftNode) LeaderId() uint64 {
-	return rm.leaderId
+	return rn.leaderId
 }
 
 func (rn *RaftNode) Shutdown() {
@@ -141,9 +148,3 @@ func (rn *RaftNode) Shutdown() {
 	rn.sm.Stop()
 	//Stop the machine
 }
-
-
-
-
-
-
